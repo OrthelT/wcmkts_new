0589ea98efad629c21a22071c7b4fac43c9aab67
Orthel Toralen <orthel.toralen@gmail.com>
2025-11-23 18:19:50 -0500

    switch to slim wcmkt4.db

--- config.py
+++ config.py
@@ -87,10 +87,10 @@ class RWLock:
 
 class DatabaseConfig:
 
-    wcdbmap = "wcmkt2" #master config variable for the database to use
+    wcdbmap = "wcmkt4" #master config variable for the database to use
 
     _db_paths = {
-        "wcmkt2": "wcmkt2.db", #production database
+        "wcmkt4": "wcmkt4.db", #production database
         "sde": "sdelite2.db",
         "build_cost": "buildcost.db",
         "wcmkt3": "wcmkt3.db" #testing db
@@ -98,14 +98,14 @@ class DatabaseConfig:
     }
 
     _db_turso_urls = {
-        "wcmkt2_turso": st.secrets.wcmkt2_turso.url,
+        "wcmkt4_turso": st.secrets.wcmkt4_turso.url,
         "sde_turso": st.secrets.sdelite2_turso.url,
         "build_cost_turso": st.secrets.buildcost_turso.url,
         "wcmkt3_turso": st.secrets.wcmkt3_turso.url,
     }
 
     _db_turso_auth_tokens = {
-        "wcmkt2_turso": st.secrets.wcmkt2_turso.token,
+        "wcmkt4_turso": st.secrets.wcmkt4_turso.token,
         "sde_turso": st.secrets.sdelite2_turso.token,
         "build_cost_turso": st.secrets.buildcost_turso.token,
         "wcmkt3_turso": st.secrets.wcmkt3_turso.token,
