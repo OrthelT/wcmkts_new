6c5ab3ebe3489e3ad7845d2fb391a4b66613ac3b
Orthel Toralen <orthel.toralen@gmail.com>
2026-01-03 19:54:36 -0500

    refactor: Doctrine refactor phase I: domain layer

---
+++ domain/__init__.py
@@ -0,0 +1,32 @@
+"""
+Domain Models Package
+
+This package contains the core domain models for the doctrine module.
+These dataclasses provide typed, immutable structures that replace
+raw DataFrame passing throughout the codebase.
+
+Key Components:
+- Enums: StockStatus, ShipRole for categorical data
+- Models: FitItem, FitSummary, ModuleStock, Doctrine
+"""
+
+from domain.enums import StockStatus, ShipRole
+from domain.models import (
+    FitItem,
+    FitSummary,
+    ModuleStock,
+    ModuleUsage,
+    Doctrine,
+)
+
+__all__ = [
+    # Enums
+    "StockStatus",
+    "ShipRole",
+    # Models
+    "FitItem",
+    "FitSummary",
+    "ModuleStock",
+    "ModuleUsage",
+    "Doctrine",
+]
